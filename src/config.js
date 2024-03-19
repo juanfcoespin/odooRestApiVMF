dc57e7583834e9a285043ca5c70ec3f214e9c9a8
Juan Francisco Espín Navia <jespin@jbp.com.ec>
2024-03-19 16:45:13 -0500

    19Mar2024_5

--- src/config.js
+++ src/config.js
@@ -1,37 +1,33 @@
-    
-    //mac
-    /*const confDb = {
-        host: "localhost",
-        user: "mac",
-        port: 5432,
-        password: "odoo",
-        database: "vmf"
-    };*/
-
-    //node server.js (para el servidor)
-    //npm run dev (para desarrollo)
-
-    //portatil oficina
-    const confDb={
-        host: "localhost",
-        user: "openpg",
-        port: 5432,
-        password: "openpgpwd",
-        database: "vmf",
-    };
-    const confGlobal={
-        zonaHorariaUTF: -5
-    };
-
-    //servidor
-    /*const confDb={
-        host: "localhost",
-        user: "odoo17",
-        port: 5432,
-        password: "odoo17",
-        database: "vmf.jbp.com.ec"
-    };*/
-    module.exports = {
-        confDb,
-        confGlobal,
-    };
+//node server.js (para el servidor)
+//npm run dev (para desarrollo)
+const confGlobal={
+    zonaHorariaUTF: -5
+};
+//mac
+/*const confDb = {
+    host: "localhost",
+    user: "mac",
+    port: 5432,
+    password: "odoo",
+    database: "vmf"
+};*/
+//portatil oficina
+const confDb={
+    host: "localhost",
+    user: "openpg",
+    port: 5432,
+    password: "openpgpwd",
+    database: "vmf",
+};
+//servidor
+/*const confDb={
+    host: "localhost",
+    user: "odoo17",
+    port: 5432,
+    password: "odoo17",
+    database: "vmf.jbp.com.ec"
+};*/
+module.exports = {
+    confDb,
+    confGlobal,
+};
