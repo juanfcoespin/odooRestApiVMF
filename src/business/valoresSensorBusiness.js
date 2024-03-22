0b1c9ba852299849f99f05ac0b55c03dc0cbd06a
Juan Francisco Espín Navia <jespin@jbp.com.ec>
2024-03-22 14:56:48 -0500

    22Mar2024

--- src/business/valoresSensorBusiness.js
+++ src/business/valoresSensorBusiness.js
@@ -9,20 +9,7 @@ async function save(me){
         //odoo maneja la hora utf, para traer la fecha actual hay que hacer lo siguiente
         var idSensor = await sensorBusiness.getId(me);
         for(var item of me.valores){
-            //- interval '${conf.confGlobal.zonaHorariaUTF} hour'
-            //const fecha=`to_date('${item.fecha}', 'yyyy-mm-dd HH:MI:SS')`;
-            const fecha=`to_timestamp('${item.fecha}','yyyy-mm-dd HH:MI:SS') - interval '-5 hour'`
-            var sql=`
-            insert into tt_sitrad_medicion_sensor(area_sensor_id, valor, fecha)
-            values($1, $2, $3);
-            `;
-            var params=[idSensor, item.valor, fecha];
-            try{
-                var insertado = await dbUtils.execute(sql, params);
-                ms.push({id: item.id, resp:insertado});
-            }catch(e){
-                ms.push({id: item.id, resp:false, error:e});
-            }
+            await saveItem(item, ms, idSensor);
         }
         return ms;
     }catch(e){
@@ -30,6 +17,23 @@ async function save(me){
         return {
             "error": '\r\n'+'valoresSensorBusiness.save(): '+e
         };
+    }
+}
+async function saveItem(item, ms, idSensor){
+    //- interval '${conf.confGlobal.zonaHorariaUTF} hour'
+    //const fecha=`to_date('${item.fecha}', 'yyyy-mm-dd HH:MI:SS')`;
+    //const fecha=`to_timestamp('${item.fecha}','yyyy-mm-dd HH:MI:SS') - interval '-5 hour'`
+    const fecha=`'${item.fecha}' - interval '-5 hour'`
+    var sql=`
+    insert into tt_sitrad_medicion_sensor(area_sensor_id, valor, fecha)
+    values($1, $2, $3);
+    `;
+    var params=[idSensor, item.valor, item.fecha];
+    try{
+        var insertado = await dbUtils.execute(sql, params);
+        ms.push({id: item.id, resp:insertado});
+    }catch(e){
+        ms.push({id: item.id, resp:false, error:e});
     }
 }
 
