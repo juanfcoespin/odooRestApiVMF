7369c54206bbab3504a26ead07fb6fe1ea6029bb
Juan Francisco Espín Navia <jespin@jbp.com.ec>
2024-03-25 11:15:12 -0500

    25Mar2024_2

--- src/config.js
+++ src/config.js
@@ -5,21 +5,21 @@ const confGlobal={
 };
 
 //portatil oficina
-const confDb={
+/*const confDb={
     host: "localhost",
     user: "openpg",
     port: 5432,
     password: "openpgpwd",
     database: "mediciones_sitrad",
-};
+};*/
 //servidor
-/*const confDb={
+const confDb={
     host: "localhost",
     user: "odoo17",
     port: 5432,
     password: "odoo17",
     database: "sitrad.jbp.com.ec"
-};*/
+};
 module.exports = {
     confDb,
     confGlobal,
