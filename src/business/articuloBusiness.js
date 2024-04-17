bd702f0cb2d88be5d008b6b618d261bc63de55c2
Juan Francisco Espín Navia <jespin@jbp.com.ec>
2024-04-17 13:00:33 -0500

    17Abr2024

--- src/business/articuloBusiness.js
+++ src/business/articuloBusiness.js
@@ -6,7 +6,7 @@ async function getInventarioByMailRepresentante(email){
          t3.id "idArticulo",
          t3.name articulo,
          t4.name "tipoArticulo",
-         t2.cantidad
+         sum(t2.cantidad) "cantidad"
         from
          tt_visitas_bodega t0 inner join
          tt_visitas_representante t1 on t1.id=t0.responsable_id inner join
@@ -14,10 +14,15 @@ async function getInventarioByMailRepresentante(email){
          tt_visitas_articulo t3 on t3.id=t2.articulo_id inner join
          tt_visitas_tipo_articulo t4 on t4.id=t3.tipo_articulo_id
         where
-         t1.email='${email}' 
+         t1.email=$1
+         and t2.activo=true
+        group by
+		 t3.id,
+         t3.name,
+         t4.name
         `;
-
-        return await dbUtils.getRows(sql);
+        
+        return await dbUtils.getRows(sql,[email]);
     }catch(e){
         return{
             "error": '\r\getMedicosByEmailRepresentante'+e
