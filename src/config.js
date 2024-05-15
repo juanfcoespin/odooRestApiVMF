a088376c19bdf2a47f77fd9f25479da71e8d89bf
juanfcoespin <juan.espin@yahoo.com>
2024-05-15 15:57:54 -0500

    15May2024

--- src/config.js
+++ src/config.js
@@ -10,11 +10,17 @@ const confGlobal={
     user: "odoo",
     port: 5432,
     password: "odoo",
-    database: "vmf"
+    database: "vmf",
+    odoo:{
+        url: "http://localhost:1769",
+        idUser:2,
+        user: "admin",
+        pwd: "AtLsV971*"
+    } 
 };*/
 
 //portatil oficina
-const confDb={
+/*const confDb={
     host: "localhost",
     user: "openpg",
     port: 5432,
@@ -26,16 +32,22 @@ const confDb={
         user: "admin",
         pwd: "AtLsV971*"
     } 
-};
+};*/
 
 //servidor
-/*const confDb={
+const confDb={
     host: "localhost",
     user: "odoo17",
     port: 5432,
     password: "odoo17",
-    database: "vmf.jbp.com.ec"
-};*/
+    database: "vmf.jbp.com.ec",
+    odoo:{
+        url: "http://vmf.jbp.com.ec:1769",
+        idUser:2,
+        user: "admin",
+        pwd: "AtLsV971*"
+    } 
+};
 module.exports = {
     confDb,
     confGlobal,
